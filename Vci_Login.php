87b186246b06fb51351594457ecb03bb48aff2ba
adityac0620 <adityac0620@gmail.com>
2017-03-16 12:04:36 +0530

    Create Vci_Login.php
    Basic  Framework  for the login page .

---
+++ Vci_Login.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>Vivid Cipher Login</title>
+</head>
+<body>
+  <form  action="val.php" method="post">
+    <input id="FUn" name="uname" type="text" placeholder="Username"/ required="true"><br/>
+    <input id="FPw" name="pword" type="password" placeholder="Password" required="true"/><br/>
+    <input type="submit" value="Login"/>
+  </form>
+</body>
+</html>
