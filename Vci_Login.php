dd71db0448b9b5ebd55d921936b16273409aa838
adityac0620 <adityac0620@gmail.com>
2017-03-16 12:14:38 +0530

    Update Vci_Login.php

--- Vci_Login.php
+++ Vci_Login.php
@@ -4,8 +4,8 @@
 </head>
 <body>
   <form  action="val.php" method="post">
-    <input id="FUn" name="uname" type="text" placeholder="Username"/ required="true"><br/><!-- The Username And Password Feild Doesn't Require A Label As Placeholder Does The Job.-->
-    <input id="FPw" name="pword" type="password" placeholder="Password" required="true"/><br/>
+    <input id="formUsername" name="uname" type="text" placeholder="Username"/ required="true"><br/><!-- The Username And Password Feild Doesn't Require A Label As Placeholder Does The Job.-->
+    <input id="formPassworrd" name="pword" type="password" placeholder="Password" required="true"/><br/>
     <input type="submit" value="Login"/>
   </form>
 </body>
